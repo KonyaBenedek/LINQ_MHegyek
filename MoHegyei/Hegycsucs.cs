12fb038b8af0d442408bde1c1cd6b25ab45b5c67
kbeni <benedekkonya005@gmail.com>
2023-05-18 13:10:51 +0200

    Add project files.

---
+++ MoHegyei/Hegycsucs.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoHegyei
+{
+    public class Hegycsucs
+    {
+        private string hegy;
+        private string hegyseg;
+        private int magassag;
+
+        public Hegycsucs(string hegy, string hegyseg, int magassag)
+        {
+            this.hegy = hegy;
+            this.hegyseg = hegyseg;
+            this.magassag = magassag;
+        }
+    }
+}
---
+++ MoHegyei/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Linq;
+using System.Collections.Generic;
+using System.IO;
+using MoHegyei;
+using System.Linq;
+
+
+namespace MoHegyei
+{
+    internal class Program
+    {
+        static List<Hegycsucs> csucsok = new List<Hegycsucs>();
+
+        static void Main(string[] args)
+        {
+            Feladat2();
+            Console.WriteLine("3. feladat: Hegycsúcsok száma: {0} db", csucsok.Count);
+            Console.WriteLine("4. feladat: Hegycsúcsok átlagos magassága: {0:0.00} m", Feladat4());
+            Console.WriteLine("5. feladat: A legmagasabb hegycsúcs adatai: ");
+            Feladat5();
+            Console.Write("6. feladat: Kérek egy magasságot: ");
+            int Magassag = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("\t{0}", Feladat6(Magassag));
+            Console.WriteLine("7. feladat: 3000 lábnál magasabb hegycsúcsok száma: {0}", Feladat7());
+            Console.WriteLine("8. feladat: Hegység statisztika");
+            Feladat8();
+            Console.WriteLine("9. feladat: bukk-videk.txt");
+            Feladat9();
+        }
+    }
+}
